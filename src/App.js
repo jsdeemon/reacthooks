6616b4c9e33b1dac5286923437da1b8bbc22a5d0
jsdeemon <jsdeemon@gmail.com>
2021-09-09 13:41:26 +0500

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import useInput from "./hooks/useInput";
+import Hover from "./components/Hover";
+import {useRef, useState} from "react";
+import useHover from "./hooks/useHover";
+import List from "./components/List";
+import useDebounce from "./hooks/useDebounce";
+import axios from 'axios';
+import useRequest from "./hooks/useRequest";
 
 function App() {
+    const [todos, loading, error] = useRequest(fetchTodos)
+  function fetchTodos() {
+    return axios.get(`https://jsonplaceholder.typicode.com/todos`)
+  }
+
+  if (loading) {
+      return <h1>Идет загрузка</h1>
+  }
+
+  if (error) {
+      return <h1>Произошла ошибка при загрузке данных</h1>
+  }
+
+//     const [value, setValue] = useState('');
+//
+//     const debouncedSearch = useDebounce(search, 500);
+//
+//     function search(query) {
+//         fetch(`https://jsonplaceholder.typicode.com/todos?query=` + query)
+//             .then(response => response.json())
+//             .then(json => {
+//                 console.log(json)
+//             })
+//     }
+//
+//     const onChange = (e) => {
+//         setValue(e.target.value)
+//         debouncedSearch(e.target.value)
+//     }
+//
+// const username = useInput('');
+// const password = useInput('');
+//
+// const ref = useRef();
+// const isBlackHover = useHover(ref);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>
+            {todos && todos.map((todo, index) =>
+                <div
+                    key={index}
+                    style={{padding: 30, border: '2px solid black'}}>{todo.id}. {todo.title}</div>
+            )}
+            <div
+                style={{height: 20, background: 'green'}} />
+        </div>
+      {/*<input {...username} type="text" placeholder="username" />*/}
+      {/*  <input {...password} type="text" placeholder="password" />*/}
+
+      {/*<button onClick={() => console.log(username.value, password.value)}>Click</button>*/}
+
+        {/*<Hover />*/}
+        {/*<div ref={ref} style={{width: 300, height: 300, background: isBlackHover ? 'gray' : 'black'}}>*/}
+        {/*</div>*/}
+        {/*<List />*/}
+        {/*<input type="text"*/}
+        {/*value={value}*/}
+        {/*       onChange={onChange}*/}
+        {/*/>*/}
+
     </div>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,40 @@
+import React, {useEffect, useRef, useState} from 'react';
+import useScroll from "../hooks/useScroll";
+
+const List = () => {
+
+    const [todos, setTodos] = useState([])
+
+    // динамическая пагинация
+    const [page, setPage] = useState(1);
+    const limit = 20;
+    const parentRef = useRef();
+    const childRef = useRef();
+    const intersected = useScroll(parentRef, childRef, () => fetchTodos(page, limit));
+
+    function fetchTodos(page, limit) {
+        fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}&_page=${page}`)
+            .then(response => response.json())
+            .then(json => {
+                setTodos(prev => [...prev, ...json]);
+                setPage(prev => prev + 1)
+            })
+    }
+
+
+// для полосы прокрутки нужно ставить стиль CSS overflow: auto
+    return (
+        <div ref={parentRef} style={{height: '80vh', overflow: 'auto'}}>
+            {todos.map((todo, index) =>
+            <div
+                key={index}
+                style={{padding: 30, border: '2px solid black'}}>{todo.id}. {todo.title}</div>
+            )}
+            <div
+                ref={childRef}
+                style={{height: 20, background: 'green'}} />
+        </div>
+    );
+};
+
+export default List;
